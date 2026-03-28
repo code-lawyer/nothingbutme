2d12709569a62a979f0a682ce537f3ccfe332e12
code-lawyer <lanzhouda@gmail.com>
2026-03-28 21:26:01 +0800

    feat: add About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-// app/page.tsx
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
